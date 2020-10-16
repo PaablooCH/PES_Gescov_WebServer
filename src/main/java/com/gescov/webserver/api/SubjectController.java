e41af745dfd8b7ad4e60f6e4386d5aceeb846171
Victorsitu <victor.martinez.m@estudiantat.upc.edu>
2020-10-16 19:31:16 +0200

    Materia 1.0

---
+++ src/main/java/com/gescov/webserver/api/SubjectController.java
@@ -0,0 +1,48 @@
+package com.gescov.webserver.api;
+
+
+import com.gescov.webserver.model.Subject;
+import com.gescov.webserver.service.SubjectService;
+import com.mongodb.lang.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/subject")
+@RestController
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    @Autowired
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @PostMapping
+    public void addSubject(@NonNull @RequestBody Subject subject){
+        subjectService.addSubject(subject);
+    }
+
+    @GetMapping
+    public List<Subject> getAllSubject(){
+        return subjectService.getAllSubject();
+    }
+
+    @GetMapping(path = "{id}")
+    public Subject getSubjectById(@PathVariable("id") ObjectId id)throws Exception{
+        return subjectService.selectSubjectByID(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteSubject(@PathVariable("id") ObjectId id){
+        subjectService.deleteSubject(id);
+    }
+
+    @PostMapping(path = "{id}")
+    public void updateSubject(@PathVariable ("id") ObjectId id, @NonNull @RequestBody Subject subject){
+        subjectService.updateSubject(id, subject);
+    }
+}
--- src/main/java/com/gescov/webserver/model/Subject.java
+++ src/main/java/com/gescov/webserver/model/Subject.java
@@ -1,18 +1,21 @@
 package com.gescov.webserver.model;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "materia")
-public class Materia {
+@Document(collection = "subject")
+public class Subject {
 
     private ObjectId id;
 
     private String name;
 
-    public Materia(@JsonProperty("_id") ObjectId id, @JsonProperty("name") String name) {
+    public Subject(){
+
+    }
+
+    public Subject(@JsonProperty("_id") ObjectId id, @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
