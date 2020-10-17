a0dfb610d204a30ab551eaa0f7c07cb36997f762
Pablo CH <paabloo.ch@gmail.com>
2020-10-17 04:04:46 +0200

    SonarQuBe errors fixed

--- src/main/java/com/gescov/webserver/model/Classroom.java
+++ src/main/java/com/gescov/webserver/model/Classroom.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "classrooms")
+//@Document(collection = "classrooms")
 public class Classroom {
 
     private ObjectId id;
