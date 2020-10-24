480f318861083340b459203b9719551d63167a31
Pablo CH <paabloo.ch@gmail.com>
2020-10-24 04:25:34 +0200

    Exception insert assignment added

--- src/main/java/com/gescov/webserver/dao/AssignmentMongoDB.java
+++ src/main/java/com/gescov/webserver/dao/AssignmentMongoDB.java
@@ -1,5 +1,6 @@
 package com.gescov.webserver.dao;
 
+import com.gescov.webserver.exception.AlreadyExistsException;
 import com.gescov.webserver.model.Assignment;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
@@ -30,24 +31,36 @@ public class AssignmentMongoDB implements AssignmentDao{
     }
 
     @Override
-    public int insertAssignment(Assignment assignment) { //comprobar lugares ya ocupados en la clase
-        FindIterable<Assignment> result = assignmentCollection.find(eq("nameSt",assignment.getNameSt()));
-        boolean insert = true;
+    public int insertAssignment(Assignment assignment) { //Eficiente Funciona// @Pablo CH
+        FindIterable<Assignment> result = assignmentCollection.find(eq("classSession", assignment.getClassSession()));
+        boolean nameRepeated = false;
+        boolean posNotValid = false;
         for (Assignment as : result) {
-            if (as.getClassSession().equals(assignment.getClassSession())) {
-                insert = false;
+            if (as.getNameSt().equals(assignment.getNameSt())) {
+                nameRepeated = true;
+                break;
+            }
+            if (as.getPosCol() == assignment.getPosCol() && as.getPosRow() == assignment.getPosRow()) {
+                posNotValid = true;
                 break;
             }
         }
-        if (insert) {
-            assignmentCollection.insertOne(assignment);
-            return 1;
+
+        if (nameRepeated) {
+            throw new AlreadyExistsException("The person with 'name' " + assignment.getNameSt() +
+                    " is already in the classSession");
         }
-        return 0;
+        else if (posNotValid) {
+            throw new AlreadyExistsException("The position with 'row' " + assignment.getPosRow() + " and 'col' " +
+                    assignment.getPosCol() + " is already occupied");
+        }
+
+        assignmentCollection.insertOne(assignment);
+        return 1;
     }
 
     @Override
-    public List<Assignment> selectAllAssignment() {
+    public List<Assignment> selectAllAssignment() { //Eficiente Funciona// @Pablo CH
         List<Assignment> allAssignment = new ArrayList<>();
         FindIterable<Assignment> result = assignmentCollection.find();
         for (Assignment as : result) {
@@ -57,19 +70,19 @@ public class AssignmentMongoDB implements AssignmentDao{
     }
 
     @Override
-    public int deleteAssignment(ObjectId id) {
+    public int deleteAssignment(ObjectId id) { //Eficiente Funciona// @Pablo CH
         assignmentCollection.deleteOne(eq("id", id));
         return 1;
     }
 
     @Override
-    public int updateAssignmentRow(ObjectId id, int posRow) {
+    public int updateAssignmentRow(ObjectId id, int posRow) { //Eficiente Funciona// @Pablo CH
         assignmentCollection.findOneAndUpdate(eq("_id", id), set("posRow", posRow));
         return 1;
     }
 
     @Override
-    public int updateAssignmentCol(ObjectId id, int posCol) {
+    public int updateAssignmentCol(ObjectId id, int posCol) { //Eficiente Funciona// @Pablo CH
         assignmentCollection.findOneAndUpdate(eq("_id", id), set("posCol", posCol));
         return 1;
     }
