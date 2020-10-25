47112733decd1e88749ecf26b08b33b137c25f29
Albertbm4 <thealbertbm@gmail.com>
2020-10-25 19:40:35 +0100

    User first version with school coupled

---
+++ src/main/java/com/gescov/webserver/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.gescov.webserver.dao;
+
+import com.gescov.webserver.model.Classroom;
+import com.gescov.webserver.model.School;
+import com.gescov.webserver.model.User;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+public interface UserDao {
+
+    int insertUser(User user);
+
+    List<User> selectAllUsers();
+
+    User selectUserById(ObjectId id);
+
+    School getUserSchool(ObjectId id);
+}
---
+++ src/main/java/com/gescov/webserver/dao/UserMongoDB.java
@@ -0,0 +1,60 @@
+package com.gescov.webserver.dao;
+
+import com.gescov.webserver.exception.NotFoundException;
+import com.gescov.webserver.model.Classroom;
+import com.gescov.webserver.model.School;
+import com.gescov.webserver.model.User;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+
+@Repository("userMongo")
+public class UserMongoDB implements UserDao {
+
+    @Qualifier("mongoClient")
+    @Autowired
+    private MongoClient client;
+    private MongoCollection<User> userCollection;
+
+    @PostConstruct
+    void init() {
+        userCollection = client.getDatabase("Gescov").getCollection("users", User.class);
+    }
+
+    @Override
+    public int insertUser(User user) {
+        userCollection.insertOne(user);
+        return 1;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        List<User> allUsers = new ArrayList<>();
+        FindIterable<User> result = userCollection.find();
+        for (User us : result) allUsers.add(us);
+        return allUsers;
+    }
+
+    @Override
+    public School getUserSchool(ObjectId id) {
+        return selectUserById(id).getSchool();
+    }
+
+    @Override
+    public User selectUserById(ObjectId id) {
+        User us = userCollection.find(eq("_id", id)).first(); //first?
+        if (us == null) throw new NotFoundException("User with 'id' " + id + " not found!");
+        return us;
+    }
+
+}
