0a47b0e8c7f355459db7c03ef48511ac09dc6943
Albertbm4 <thealbertbm@gmail.com>
2020-11-08 02:16:54 +0100

    Classroom compound index name+school.id and User adapting changes

--- src/main/java/com/gescov/webserver/model/User.java
+++ src/main/java/com/gescov/webserver/model/User.java
@@ -1,55 +1,45 @@
 package com.gescov.webserver.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.bson.types.ObjectId;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@NoArgsConstructor
+@Setter
+@Getter
 public class User {
 
     @Id
-    private ObjectId id;
+    private String id;
 
+    @NotNull(message = "User's name must not be null")
     private String name;
 
-    @DBRef
+    @NotNull(message = "User's name must not be null")
+    private String email;
+
+    @NotNull(message = "User's risk must be yes (true) or no (false)")
+    private boolean risk;
+
+    @DBRef(db="schools")
     private List<School> schools;
 
-    public User() {
 
-    }
-
-    public User(@JsonProperty("id") ObjectId id,
-                     @JsonProperty("name") String name,
-                     @JsonProperty("schools") final List<School> schools) {
+    public User(@JsonProperty("id") String id,
+                @JsonProperty("name") String name,
+                @JsonProperty("email") String email,
+                @JsonProperty("risk") boolean risk,
+                @JsonProperty("schools") final List<School> schools) {
         this.id = id;
         this.name = name;
-        this.schools = schools;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<School> getSchools() {
-        return schools;
-    }
-
-    public void setSchools(List<School> schools) {
+        this.email = email;
+        this.risk = risk;
         this.schools = schools;
     }
 
