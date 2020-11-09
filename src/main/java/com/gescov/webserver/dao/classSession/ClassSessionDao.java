b9601bd62073ee91a8a5845b7fdbd077ab56ab93
Victorsitu <victor.martinez.m@estudiantat.upc.edu>
2020-11-09 21:14:55 +0100

    ClassSession refactor

--- src/main/java/com/gescov/webserver/dao/classSession/ClassSessionDao.java
+++ src/main/java/com/gescov/webserver/dao/classSession/ClassSessionDao.java
@@ -5,13 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface ClassSessionDao extends MongoRepository <ClassSession, String> {
-
-    List<ClassSession> findAllByClassroom_Id(String variable);
-
-    List<ClassSession> findAllBySubject_Id(String variable);
-
-    List<ClassSession> findAllByTeacher_Id(String variable);
+public interface ClassSessionDao extends MongoRepository <ClassSession, String> , ClassSessionCustomizedMongoRepository<ClassSession, String>{
 
     List<ClassSession> findAllByHour(String variable);
 
