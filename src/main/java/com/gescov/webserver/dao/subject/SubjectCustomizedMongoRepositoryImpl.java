18e2ae3e19dd02a087f139c919627c7ff593d52e
Victorsitu <victor.martinez.m@estudiantat.upc.edu>
2020-11-09 21:27:36 +0100

    SubjectImpl con Dao y buscar por school id

--- src/main/java/com/gescov/webserver/dao/subject/SubjectCustomizedMongoRepositoryImpl.java
+++ src/main/java/com/gescov/webserver/dao/subject/SubjectCustomizedMongoRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.gescov.webserver.dao.subject;
 
+import com.gescov.webserver.dao.school.SchoolDao;
 import com.gescov.webserver.model.School;
 import com.gescov.webserver.model.Subject;
 import com.gescov.webserver.service.SchoolService;
@@ -10,21 +11,29 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository("subjectMongo")
 public class SubjectCustomizedMongoRepositoryImpl<T,ID> implements SubjectCustomizedMongoRepository<T,ID> {
 
     private final MongoTemplate mongoTemplate;
-    private final SchoolService schoolService;
+    private final SchoolDao schoolDao;
 
     @Autowired
-    public SubjectCustomizedMongoRepositoryImpl(MongoTemplate mongoTemplate, SchoolService schoolService) {
+    public SubjectCustomizedMongoRepositoryImpl(MongoTemplate mongoTemplate, SchoolDao schoolDao) {
         this.mongoTemplate = mongoTemplate;
-        this.schoolService = schoolService;
+        this.schoolDao = schoolDao;
+    }
+
+    public List<Subject> selectAllBySchoolId (String id){
+        Optional<School> school = schoolDao.findById(id);
+        Query q = new Query();
+        q.addCriteria(Criteria.where("schoolID").is(school.get().getId()));
+        return mongoTemplate.find(q, Subject.class);
     }
 
     public List<Subject> selectAllBySchoolName (String schoolName){
-        School school = schoolService.getSchoolByName(schoolName);
+        School school = schoolDao.findByName(schoolName);
         Query q = new Query();
         q.addCriteria(Criteria.where("schoolID").is(school.getId()));
         return mongoTemplate.find(q, Subject.class);
